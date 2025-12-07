7bf187a66760ea79f0129c73abca9cf5073e9ffa
aryrdnsyh <aryrdnsyh@gmail.com>
2025-12-07 21:04:37 +0700

    Merge remote branch main

--- app/src/main/java/com/example/projectkelompok/MainActivity.kt
+++ app/src/main/java/com/example/projectkelompok/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.projectkelompok
+package com.edulearn.kelompok3
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -10,11 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        // setContentView(R.layout.activity_main)  // <-- Beri komentar di sini
-        // ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets -> // <-- Dan di sini
-        //     val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-        //     v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-        //     insets
-        // }
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
